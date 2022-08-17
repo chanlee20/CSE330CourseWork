5c3337f54fed6260483fc0fc3a19ac280bd5e420
Chan Lee <chan.l1@wustl.edu>
2022-08-17 14:41:18 +0900

    Update connect.php

--- module3/connect.php
+++ module3/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$mysqli = new mysqli('localhost', 'chanlee20', 'wlwl5909', 'module3');
+$mysqli = new mysqli('localhost', 'chanlee20', '', 'module3');
 
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
